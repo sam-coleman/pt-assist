391d81837965b3346cf159f99fdeab1efee1f681
reisenbud <ruby.eisenbud@gmail.com>
2020-12-11 13:48:57 -0500

    Create algorithm_draft.m

---
+++ algorithm_draft.m
@@ -0,0 +1,59 @@
+% function result = form_correct(filename,maybe something else)
+addpath('/Data')
+trial = filter_data('B3');
+x_rms = rms(trial(:,2))
+y_rms = rms(trial(:,3))
+z_rms = rms(trial(:,4))
+
+x_thresh = 32;
+y_thresh = 41;
+z_thresh = 30;
+
+if (x_rms > x_thresh || y_rms > y_thresh || z_rms > z_thresh)
+    if (x_rms > x_thresh) && (y_rms <= y_thresh) && (z_rms <= z_thresh)  %just x wrong
+        disp('wrong x')
+    end
+    if (y_rms > y_thresh) && (x_rms <= x_thresh) && (z_rms <= z_thresh)  %just y wrong
+        disp('wrong y')
+    end
+    if (z_rms > z_thresh) && (x_rms <= x_thresh) && (y_rms <= y_thresh)  %just z wrong
+        disp('wrong z')
+    end
+    if (x_rms > x_thresh) && (y_rms > y_thresh) && (z_rms <= z_thresh)  %x and y wrong
+        disp('wrong x and y')
+    end
+    if (y_rms > y_thresh) && (x_rms <= x_thresh) && (z_rms > z_thresh)  %y and z wrong
+        disp('wrong y and z')
+    end
+    if (z_rms > z_thresh) && (x_rms > x_thresh) && (y_rms <= y_thresh)  %x and z wrong
+        disp('wrong x and z')
+    end
+    if (x_rms > x_thresh) && (y_rms > y_thresh) && (z_rms > z_thresh)  %all three wrong
+        disp('wrong x')
+    end
+else
+    disp('Nice! Your form was correct.')
+end
+
+
+
+%end
+
+
+
+
+
+function fft_data = filter_data(file_name)
+data_time = ParseMatlabAppFinal(file_name);
+data_filt = move_avg_filt(data_time, 5);
+fft_data = fft_from_time(data_filt, 100);
+fft_data = remove_0(fft_data);
+end
+
+function freq_data = remove_0(freq_data)
+    
+    for i=2:4
+        [~, Index] = max(freq_data(:,i));
+        freq_data(Index, i) = 0;
+    end
+end
